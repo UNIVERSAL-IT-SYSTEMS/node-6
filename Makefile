f115572fb1b03cacc5aedb2145ee6c52498ddb35
Patrick Heneise <patrick@fastmail.im>
2015-12-01 16:27:19 +0100

    relaunch

--- Makefile
+++
@@ -1,9 +0,0 @@
-TESTS = tests/test-*.js
-REPORTER = spec
-
-test:
-	@./node_modules/.bin/mocha \
-		--require should \
-		--reporter $(REPORTER) \
-		--timeout 2000 \
-		$(TESTS)
--- common/config.js
+++
@@ -1,9 +0,0 @@
-var config;
-
-if (process.env.NODE_ENV === 'production') {
-  config = require('./production');
-} else {
-  config = require('./development');
-}
-
-module.exports = exports = config;
